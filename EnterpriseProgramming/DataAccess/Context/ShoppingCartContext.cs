5f3558e122144db934c151dfc9c98efdd1fd3c5a
Ryan Attard <ryanattard@gmail.com>
2022-10-05 10:51:33 +0200

    Context started construction

--- EnterpriseProgramming/DataAccess/Context/ShoppingCartContext.cs
+++ EnterpriseProgramming/DataAccess/Context/ShoppingCartContext.cs
@@ -1,15 +1,26 @@
-﻿using System;
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Context
 {
-    public class ShoppingCartContext //: IdentityDbContext
+    public class ShoppingCartContext : IdentityDbContext
     {
-      /*  public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
           : base(options)
         {
         }
-      */
+
+
+        //1. autonumber for ids
+        //2. commands to generate the db
+
+        //names you are giving to these properties, will be used for the table names
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Category> Categories { get; set; }
+      
     }
 }
