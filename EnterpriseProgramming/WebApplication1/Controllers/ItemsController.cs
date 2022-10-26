9187187d452b81fe3e0ef80a789c964c1e4ecdbb
Ryan Attard <ryanattard@gmail.com>
2022-10-26 10:54:33 +0200

    Image Upload

--- EnterpriseProgramming/WebApplication1/Controllers/ItemsController.cs
+++ EnterpriseProgramming/WebApplication1/Controllers/ItemsController.cs
@@ -1,8 +1,11 @@
 ﻿using BusinessLogic.Services;
 using BusinessLogic.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,10 +20,12 @@ namespace WebApplication1.Controllers
     {
         private ItemsService itemsService;
         private CategoriesService categoriesService;
-        public ItemsController (ItemsService _itemsService, CategoriesService _categoriesService)
+        private IWebHostEnvironment hostService;
+        public ItemsController (ItemsService _itemsService, CategoriesService _categoriesService, IWebHostEnvironment _host)
         {
             itemsService = _itemsService;
             categoriesService = _categoriesService;
+            hostService = _host;
                  
         }
 
@@ -45,16 +50,50 @@ namespace WebApplication1.Controllers
         }
 
         [HttpPost] //Post method is called after the end user fills in the data and presses Submit button
-        public IActionResult Create(CreateItemViewModel data)
+        public IActionResult Create(CreateItemViewModel data, IFormFile file)
         {
             //......
             try
             {
+                //----------------Image upload------------------------------
+
+
+                //1. to check whether image has been received successfully 
+                if (file != null)
+                {
+
+                    //2. generate a unique filename for the image
+                    string uniqueFilename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                    //3. identify a place (in the wwwroot > Images) where to store the actual physical file
+                    string absolutePath = hostService.WebRootPath + @"\Images\" + uniqueFilename;
+
+                    //4. save the actual file
+                    using (var stream = System.IO.File.Create(absolutePath)) //this will create an empty stream at location : absolutePath
+                    {
+                          file.CopyTo(stream); //this will copy the data from the uploaded file (parameter) into  empty stream at absolute Path
+                    } //will close all streams
+
+                    //5. form the ImagePath and store assign it in the model above
+
+                    data.ImagePath = "/Images/" + uniqueFilename; //the first / indicates asp.net to start locating the image from the root folder
+
+                }
+
+                //-------------------------------------------------
+
                 //1. implement the create method by
                 //2. applying dependency injection and ask for ItemsService
                 //3. itemsService.AddNewItem(......)
                 itemsService.AddNewItem(data.Name, data.Price, data.CategoryId, data.Stock, data.ImagePath);
-                 
+
+
+                //ViewBag doesn't survive redirections
+              //  TempData["Message"] = "Item added successfully";
+
+                //ViewBag vs Tempdata
+                //ViewBag doesnt survive redirections
+                //Tempdata survives one redirection
 
                 ViewBag.Message = "Item added successfully";
             }
@@ -65,10 +104,12 @@ namespace WebApplication1.Controllers
                 ViewBag.Error = "There was a problem adding a new item. make sure all the fields are correctly filled";
             }
 
+            // return RedirectToAction("Create");
+            var categories = categoriesService.GetCategories();
+            CreateItemViewModel myModel = new CreateItemViewModel();
+            myModel.Categories = categories;
 
-
-
-            return RedirectToAction("Create");
+            return View("Create", myModel);
         }
 
         public IActionResult List()
